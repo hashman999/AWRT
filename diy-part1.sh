5334f7b38f28fc2935b2d6fad5eb66bdfb08294b
hashman999 <bee.super.wallet@gmail.com>
2025-03-14 14:40:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,6 +9,8 @@
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
 sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' ./feeds.conf.default
 sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' ./feeds.conf.default
+sed -i '$a src-git modem https://github.com/FUjr/modem_feeds.git' ./feeds.conf.default
+sed -i '$a src-git 5GModem https://github.com/mdsdtech/5G-Modem-Packages.git' ./feeds.conf.default
 
 # homeproxy
 git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
