9bd4ccaf05f8567d16daf87aeb12bf3bda40c860
hashman999 <bee.super.wallet@gmail.com>
2025-03-15 22:01:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,8 +3,8 @@
 # 添加额外 feed 源
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
 #sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' ./feeds.conf.default
-#sed -i '$a src-git modem https://github.com/FUjr/modem_feeds.git' ./feeds.conf.default
-#sed -i '$a src-git 5GModem https://github.com/mdsdtech/5G-Modem-Packages.git' ./feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small-package.git' ./feeds.conf.default
-#sed -i '$a src-git openappfilter https://github.com/destan19/OpenAppFilter.git' ./feeds.conf.default
+sed -i '$a src-git modem https://github.com/FUjr/modem_feeds.git' ./feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages.git' ./feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small-package.git' ./feeds.conf.default
+sed -i '$a src-git src-git jell https://github.com/kenzok8/jell.git' ./feeds.conf.default
 
