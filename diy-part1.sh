c7412bc289cd46135a7464814b6c26fc804e890d
hashman999 <bee.super.wallet@gmail.com>
2025-03-26 17:29:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,5 +6,6 @@ sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.c
 sed -i '$a src-git modem https://github.com/FUjr/modem_feeds.git' ./feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages.git' ./feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small-package.git' ./feeds.conf.default
+sed -i '$a src-git fancontrol https://github.com/JiaY-shi/fancontrol.git' ./feeds.conf.default
 #sed -i '$a src-git jell https://github.com/kenzok8/jell.git' ./feeds.conf.default
 
