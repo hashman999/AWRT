5b3b99458488816b685215a6b11499dff47a08e8
hashman999 <bee.super.wallet@gmail.com>
2025-03-29 19:41:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,7 +2,7 @@
 
 # 添加额外 feed 源
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
-sed -i '$a src-git 5gmodem https://github.com/Siriling/5G-Modem-Support.git' ./feeds.conf.default
+#sed -i '$a src-git 5gmodem https://github.com/Siriling/5G-Modem-Support.git' ./feeds.conf.default
 sed -i '$a src-git modem https://github.com/FUjr/modem_feeds.git' ./feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages.git' ./feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small-package.git' ./feeds.conf.default
