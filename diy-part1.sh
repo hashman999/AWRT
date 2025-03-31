68caf182c7f0740b75b4ef1fc615f03ff86441d5
hashman999 <bee.super.wallet@gmail.com>
2025-03-31 08:34:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,8 +9,4 @@ sed -i '$a src-git small https://github.com/kenzok8/small-package.git' ./feeds.c
 sed -i '$a src-git fancontrol https://github.com/JiaY-shi/fancontrol.git' ./feeds.conf.default
 #sed -i '$a src-git jell https://github.com/kenzok8/jell.git' ./feeds.conf.default
 
-# ===== 【重点】锁定 NodeJS 版本到 20.17.0，避免 cares_wrap.h 报错 =====
-echo "回退 NodeJS feed 到 v20.17.0，防止 c-ares API 编译失败"
-cd feeds/packages/lang/node
-git checkout 5f06f36
-cd ../../../..
+
